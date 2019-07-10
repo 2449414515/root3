b0fe91e8d1f0a3a2594580aecd2c6d1351414646
root <2449414515@qq.com>
2019-07-10 20:12:19 +0800

    添加test工程

---
+++ xiaohuihui/src/com/itheima/MyGit.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class MyGit {
+    public static void main(String[] args) {
+        System.out.println("打死小灰灰");
+        System.out.println("打死小灰灰");
+        System.out.println("打死小灰灰");
+        System.out.println("打死小灰灰");
+        System.out.println("打死小灰灰");
+    }
+}
