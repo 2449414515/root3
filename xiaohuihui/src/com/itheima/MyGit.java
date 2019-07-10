91201ffbe2eacb12a6cc4589578895a526809c6d
2449414515 <52734259+2449414515@users.noreply.github.com>
2019-07-10 20:22:07 +0800

    Update MyGit.java

--- xiaohuihui/src/com/itheima/MyGit.java
+++ xiaohuihui/src/com/itheima/MyGit.java
@@ -2,10 +2,10 @@ package com.itheima;
 
 public class MyGit {
     public static void main(String[] args) {
-        System.out.println("打死小灰灰");
-        System.out.println("打死小灰灰");
-        System.out.println("打死小灰灰");
-        System.out.println("打死小灰灰");
-        System.out.println("打死小灰灰");
+        System.out.println("打死小灰灰1");
+        System.out.println("打死小灰灰2");
+        System.out.println("打死小灰灰3");
+        System.out.println("打死小灰灰4");
+        System.out.println("打死小灰灰5");
     }
 }
